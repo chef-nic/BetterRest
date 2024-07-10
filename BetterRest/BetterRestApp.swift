25daeb8fa40c4376381e27658d870b029485379a
chef-nic <nicholasajohnson@me.com>
2024-07-10 15:53:20 -0700

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Nicholas Johnson on 7/10/24.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
